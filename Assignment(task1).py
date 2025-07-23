4f9d838f182f1bc24eded5334090f874024e7684
Mascarenhas46704 <116151547+Mascarenhas46704@users.noreply.github.com>
2025-07-23 21:41:27 +0530

    Add files via upload

---
+++ Assignment(task1).py
@@ -0,0 +1,9 @@
+#Task1
+#Check if a number is Even or Odd
+
+
+n=int(input("Enter a number:"))
+if n%2==0:
+    print(f"{n} is a even number.")
+else:
+    print(f"{n} is a odd number.")
---
+++ Assignmnet(Task2).py
@@ -0,0 +1,8 @@
+#Task2
+#Sum of integers fro, 1 to 50 using a Loop
+
+
+total=0
+for i in range(1,51):
+    total=i+total
+print("The sum of numbers from 1 to 50 is:",total)
